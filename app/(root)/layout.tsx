ba863655c93243a88f865d92abd51093def0046c
Tessio94 <simeklapan@gmail.com>
2025-11-23 01:05:55 +0100

    added change to jsonld

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,44 +4,44 @@ import ScrollWrapper from "@/components/ScrollWrapper";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-	const jsonLd = {
-		"@context": "https://schema.org",
-		"@type": "Organization",
-		name: "NLS Tech",
-		url: "https://nlstech.pro",
-		logo: "https://nlstech.pro/nls-logo.svg",
-		image: "https://nlstech.pro/landing-desktop.jpg",
-	};
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "NLS Tech",
+    url: "https://nlstech.pro",
+    logo: "https://nlstech.pro/favicon.ico",
+    image: "https://nlstech.pro/landing-desktop.jpg",
+  };
 
-	const jsonLdPro = {
-		"@context": "https://schema.org",
-		"@type": "WebPage",
-		name: "NLS Tech",
-		url: "https://nlstech.pro",
-		logo: "https://nlstech.pro/nls-logo.svg",
-		image: "https://nlstech.pro/landing-desktop.jpg",
-	};
+  const jsonLdPro = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    name: "NLS Tech",
+    url: "https://nlstech.pro",
+    logo: "https://nlstech.pro/favicon.ico",
+    image: "https://nlstech.pro/landing-desktop.jpg",
+  };
 
-	return (
-		<>
-			<script
-				type="application/ld+json"
-				dangerouslySetInnerHTML={{
-					__html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
-				}}
-			/>
-			<script
-				type="application/ld+json"
-				dangerouslySetInnerHTML={{
-					__html: JSON.stringify(jsonLdPro).replace(/</g, "\\u003c"),
-				}}
-			/>
-			<Header />
-			<>{children}</>
-			<Footer />
-			<ScrollWrapper />
-		</>
-	);
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+        }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLdPro).replace(/</g, "\\u003c"),
+        }}
+      />
+      <Header />
+      <>{children}</>
+      <Footer />
+      <ScrollWrapper />
+    </>
+  );
 };
 
 export default Layout;
