dcb133232f2ff868e330a8b3d51cee3d609b1c64
Tessio94 <simeklapan@gmail.com>
2025-12-12 11:01:11 +0100

    added changes to landing page for load optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,112 +2,127 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const montserrat = localFont({
-  src: [
-    {
-      path: "fonts/Montserrat-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "fonts/Montserrat-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "fonts/Montserrat-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "fonts/Montserrat-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "fonts/Montserrat-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-  ],
-  variable: "--mont",
+const montserratCover = localFont({
+	src: [
+		{ path: "fonts/Montserrat-Regular.ttf", weight: "400", style: "normal" },
+		{ path: "fonts/Montserrat-SemiBold.ttf", weight: "600", style: "normal" },
+		{ path: "fonts/Montserrat-ExtraBold.ttf", weight: "800", style: "normal" },
+	],
+	variable: "--mont-cover",
+	preload: true,
 });
 
-const inter = localFont({
-  src: [
-    {
-      path: "fonts/Inter_18pt-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "fonts/Inter_18pt-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "fonts/Inter_18pt-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "fonts/Inter_18pt-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "fonts/Inter_18pt-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "fonts/Inter_18pt-SemiBoldItalic.ttf",
-      weight: "800",
-      style: "italic",
-    },
-    {
-      path: "fonts/Inter_18pt-BoldItalic.ttf",
-      weight: "800",
-      style: "italic",
-    },
-  ],
-  variable: "--inter",
+const montserratRest = localFont({
+	src: [
+		{
+			path: "fonts/Montserrat-Medium.ttf",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "fonts/Montserrat-Bold.ttf",
+			weight: "700",
+			style: "normal",
+		},
+	],
+	variable: "--mont-rest",
+	preload: false,
+});
+
+const interRegular = localFont({
+	src: [
+		{
+			path: "fonts/Inter_18pt-Regular.ttf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "fonts/Inter_18pt-Medium.ttf",
+			weight: "500",
+			style: "normal",
+		},
+	],
+	variable: "--inter-regular",
+	preload: true,
+});
+
+const interRest = localFont({
+	src: [
+		{
+			path: "fonts/Inter_18pt-Medium.ttf",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "fonts/Inter_18pt-SemiBold.ttf",
+			weight: "600",
+			style: "normal",
+		},
+		{
+			path: "fonts/Inter_18pt-Bold.ttf",
+			weight: "700",
+			style: "normal",
+		},
+		{
+			path: "fonts/Inter_18pt-ExtraBold.ttf",
+			weight: "800",
+			style: "normal",
+		},
+		{
+			path: "fonts/Inter_18pt-SemiBoldItalic.ttf",
+			weight: "800",
+			style: "italic",
+		},
+		{
+			path: "fonts/Inter_18pt-BoldItalic.ttf",
+			weight: "800",
+			style: "italic",
+		},
+	],
+	variable: "--inter-rest",
+	preload: false,
 });
 
 export const metadata: Metadata = {
-  title: "NLS Tech | Lasersko čišćenje Zadar",
-  description:
-    "NLS Tech nudi profesionalno lasersko čišćenje u Zadru – sigurno, učinkovito i bez oštećenja. Vraćamo izvornu čistoću kamena, metala, betona i drugih površina.",
-  // icons: {
-  // 	icon: "/nls-logo.svg",
-  // },
-  openGraph: {
-    title: "NLS Tech | Lasersko čišćenje Zadar",
-    description:
-      "Profesionalno lasersko čišćenje tvrdih površina u Zadru – precizno, učinkovito i bez oštećenja materijala. Idealno za kamen, metal, beton i druge površine.",
-    url: "https://www.nlstech.pro/",
-    images: [
-      {
-        url: "https://www.nlstech.pro/landing-desktop.jpg",
-        width: 1920,
-        height: 1080,
-        alt: "Grad Zadar",
-      },
-    ],
-    type: "website",
-  },
+	title: "NLS Tech | Lasersko čišćenje Zadar",
+	description:
+		"NLS Tech nudi profesionalno lasersko čišćenje u Zadru – sigurno, učinkovito i bez oštećenja. Vraćamo izvornu čistoću kamena, metala, betona i drugih površina.",
+	openGraph: {
+		title: "NLS Tech | Lasersko čišćenje Zadar",
+		description:
+			"Profesionalno lasersko čišćenje tvrdih površina u Zadru – precizno, učinkovito i bez oštećenja materijala. Idealno za kamen, metal, beton i druge površine.",
+		url: "https://www.nlstech.pro/",
+		images: [
+			{
+				url: "https://www.nlstech.pro/landing-desktop.jpg",
+				width: 1920,
+				height: 1080,
+				alt: "Grad Zadar",
+			},
+		],
+		type: "website",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="hr">
-      <body className={`${montserrat.className} ${inter.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="hr">
+			<body
+				className={`
+                  ${interRegular.variable}
+                  ${interRest.variable}
+                  ${montserratCover.className}
+                  ${montserratCover.variable}
+                  ${montserratRest.variable}
+                  antialiased
+                `}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
--- components/Cover.tsx
+++ components/Cover.tsx
@@ -8,51 +8,52 @@ import { FaArrowDown } from "react-icons/fa";
 import { AnimateText } from "./AnimateText";
 
 const Cover = () => {
-  return (
-    <section className="max-[400px]:relative lg:bg-none bg-[url('/landing-mobile.jpg')] bg-no-repeat bg-cover h-screen lg:h-auto lg:min-h-[unset] min-h-[800px]">
-      <Image
-        className="z-0 cover-image lg:h-auto lg:object-contain hidden lg:block"
-        src={laser}
-        alt="Rad laserskim čistaćem"
-        width={1920}
-        height={1080}
-      />
-      <div className="xsm:gap-4 xsm:p-5 absolute z-0 flex  flex-col items-center  rounded-2xl p-6  gap-8 top-[50%] left-[50%] translate-y-[-50%] lg:translate-y-[-80%] translate-x-[-50%]  xl:translate-y-[-50%] w-[95%] lg:w-[80%]">
-        <h1 className="text-white text-5xl sm:text-6xl font-extrabold tracking-widest uppercase xl:text-7xl font-mont text-center max-[360px]:text-4xl">
-          <AnimateText text="NLS Tech" />
-        </h1>
-        <motion.p
-          className="font-inter text-2xl text-white xl:text-3xl text-center"
-          initial={{ x: -200, opacity: 0 }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            transition: { duration: 0.5 },
-          }}
-        >
-          Lasersko čišćenje tvrdih površina predstavlja modernu, neinvazivnu
-          metodu uklanjanja nečistoća, oksidacije, boje ili korozije pomoću
-          usmjerenog laserskog snopa.
-        </motion.p>
-        <motion.div
-          initial={{ y: 100, opacity: 0 }}
-          animate={{
-            y: 0,
-            opacity: 1,
-            transition: { duration: 0.5 },
-          }}
-        >
-          <a
-            href="#info"
-            className="bg-theme1/50 group xsm:py-2 flex cursor-pointer items-center gap-3 rounded-2xl border-2 border-transparent px-5 py-1 text-2xl text-white transition-border duration-200 hover:border-white focus:border-white active:border-white lg:duration-500 xl:text-3xl font-mont transform-gpu will-change-[transform,opacity] text-center"
-          >
-            Više informacija{" "}
-            <FaArrowDown className="transition-all duration-200 group-hover:translate-y-1 group-focus:translate-y-1 group-active:translate-y-1 lg:duration-500" />
-          </a>
-        </motion.div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="max-[400px]:relative lg:bg-none bg-[url('/landing-mobile.jpg')] bg-no-repeat bg-cover h-screen lg:h-auto lg:min-h-[unset] min-h-[800px]">
+			<Image
+				className="z-0 cover-image lg:h-auto lg:object-contain hidden lg:block"
+				src={laser}
+				alt="Rad laserskim čistaćem"
+				width={1920}
+				height={1080}
+				preload={true}
+			/>
+			<div className="xsm:gap-4 xsm:p-5 absolute z-0 flex  flex-col items-center  rounded-2xl p-6  gap-8 top-[50%] left-[50%] translate-y-[-50%] lg:translate-y-[-80%] translate-x-[-50%]  xl:translate-y-[-50%] w-[95%] lg:w-[80%]">
+				<h1 className="text-white text-5xl sm:text-6xl font-extrabold tracking-widest uppercase xl:text-7xl font-mont text-center max-[360px]:text-4xl">
+					<AnimateText text="NLS Tech" />
+				</h1>
+				<motion.p
+					className="font-inter text-2xl text-white xl:text-3xl text-center"
+					initial={{ x: -200, opacity: 0 }}
+					animate={{
+						x: 0,
+						opacity: 1,
+						transition: { duration: 0.5 },
+					}}
+				>
+					Lasersko čišćenje tvrdih površina predstavlja modernu, neinvazivnu
+					metodu uklanjanja nečistoća, oksidacije, boje ili korozije pomoću
+					usmjerenog laserskog snopa.
+				</motion.p>
+				<motion.div
+					initial={{ y: 100, opacity: 0 }}
+					animate={{
+						y: 0,
+						opacity: 1,
+						transition: { duration: 0.5 },
+					}}
+				>
+					<a
+						href="#info"
+						className="bg-theme1/50 group xsm:py-2 flex cursor-pointer items-center gap-3 rounded-2xl border-2 border-transparent px-5 py-1 text-2xl text-white transition-border duration-200 hover:border-white focus:border-white active:border-white lg:duration-500 xl:text-3xl font-mont transform-gpu will-change-[transform,opacity] text-center"
+					>
+						Više informacija{" "}
+						<FaArrowDown className="transition-all duration-200 group-hover:translate-y-1 group-focus:translate-y-1 group-active:translate-y-1 lg:duration-500" />
+					</a>
+				</motion.div>
+			</div>
+		</section>
+	);
 };
 
 export default Cover;
